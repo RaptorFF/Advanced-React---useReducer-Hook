d8e141a57073c89e6d38892e3ca4962a98969e06
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-12-02 13:01:50 +0100

    Update Options.js

--- src/Options.js
+++ src/Options.js
@@ -1,16 +1,21 @@
-function Options({ question, dispatch, answer }) {
+import { useQuiz } from "../contexts/QuizContext";
+
+function Options({ question }) {
+  const { dispatch, answer } = useQuiz();
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
           className={`btn btn-option ${index === answer ? "answer" : ""} ${
-            answer
+            hasAnswered
               ? question.correctOption === index
                 ? "correct"
                 : "wrong"
               : ""
           }`}
           key={option}
+          disabled={hasAnswered}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
           {option}
